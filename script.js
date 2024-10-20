39cdad0d829afa3a30212ff0d03d8e7d8a8ea315
juanito777tt <149965418+juanito777tt@users.noreply.github.com>
2024-10-20 12:11:20 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-// Add interactivity for dropdown menu
-document.addEventListener("DOMContentLoaded", function() {
-    var dropdown = document.querySelector(".dropdown");
-
-    dropdown.addEventListener("click", function() {
-        var dropdownContent = this.querySelector(".dropdown-content");
-        dropdownContent.classList.toggle("show");
-    });
-});
 
